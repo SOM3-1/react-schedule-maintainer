814a22460577bcbbb2c60360b7253f3807e1acce
Some1 <dushgowda@gmail.com>
2021-08-14 23:52:09 +0530

    adding second

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,14 +1,11 @@
 import "./Expense.css";
-export function Expense() {
-    const expenseDate= new Date(2021, 2 ,28);
-    const expenseTitle = "Insurance";
-    const expenseAmount  = 369.6;
+import { ExpenseDate } from "./ExpenseDate";
+export function Expense(data) {
   return (
-    <div className="expense-item">
-      <div> {expenseDate.toISOString()} </div>
-      <div className="expense-item__description">
-        <h2> {expenseTitle}</h2> 
-        <div className="expense-item__price"> {expenseAmount} </div>
+    <div >
+      <ExpenseDate date={data.date}/>
+      <div className="expense-item__description"><h2> {data.title}</h2>
+        <div className="expense-item__price"> {data.ammount} </div>
       </div>
     </div>
   );
